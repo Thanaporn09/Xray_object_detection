7bb04696cd51ee802f64054c437126cd025f6cd4
Thanaporn09 <55653594+Thanaporn09@users.noreply.github.com>
2022-02-08 13:20:05 +0900

    Add files via upload

---
+++ mmdet/datasets/__init__.py
@@ -0,0 +1,30 @@
+from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .cityscapes import CityscapesDataset
+from .coco import CocoDataset
+from .custom import CustomDataset
+from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
+                               RepeatDataset)
+from .deepfashion import DeepFashionDataset
+from .lvis import LVISDataset, LVISV1Dataset, LVISV05Dataset
+from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
+from .utils import (NumClassCheckHook, get_loading_pipeline,
+                    replace_ImageToTensor)
+from .voc import VOCDataset
+from .wider_face import WIDERFaceDataset
+from .xml_style import XMLDataset
+from .SIXRAY import SIXRAY_dataset
+from .LUPIN_01 import LUPIN_01
+from .LUPIN_02 import LUPIN_02
+from .LUPINV3 import LUPINV3
+from .LUPINV4 import LUPINV4
+from .LUPINV3_05 import LUPINV3_05
+
+__all__ = [
+    'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
+    'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
+    'LVISV1Dataset', 'SIXRAY_dataset','LUPIN_01','LUPIN_02','LUPINV3','LUPINV3_05','LUPINV4','GroupSampler', 'DistributedGroupSampler',
+    'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
+    'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
+    'build_dataset', 'replace_ImageToTensor', 'get_loading_pipeline',
+    'NumClassCheckHook'
+]
