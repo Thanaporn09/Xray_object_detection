afc874aecd599bcbe1718413fe63d9baac24a532
Thanaporn09 <55653594+Thanaporn09@users.noreply.github.com>
2022-02-08 13:28:47 +0900

    Add files via upload

---
+++ mmdet/core/bbox/coder/__init__.py
@@ -0,0 +1,14 @@
+from .base_bbox_coder import BaseBBoxCoder
+from .bucketing_bbox_coder import BucketingBBoxCoder
+from .delta_xywh_bbox_coder import DeltaXYWHBBoxCoder
+from .legacy_delta_xywh_bbox_coder import LegacyDeltaXYWHBBoxCoder
+from .pseudo_bbox_coder import PseudoBBoxCoder
+from .tblr_bbox_coder import TBLRBBoxCoder
+from .yolo_bbox_coder import YOLOBBoxCoder
+from .delta_xywh_bbox_coder_point import DeltaXYWHBBoxCoder_point
+
+__all__ = [
+    'BaseBBoxCoder', 'PseudoBBoxCoder', 'DeltaXYWHBBoxCoder',
+    'LegacyDeltaXYWHBBoxCoder', 'TBLRBBoxCoder', 'YOLOBBoxCoder',
+    'BucketingBBoxCoder','DeltaXYWHBBoxCoder_point'
+]
